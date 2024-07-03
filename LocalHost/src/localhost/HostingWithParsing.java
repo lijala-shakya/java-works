505a3c6d0055a2a77f991e24060827ba071ea51d
Lijala Shakya <lijalashakya01@gmail.com>
2024-07-03 18:01:29 +0545

    java-works

--- LocalHost/src/localhost/HostingWithParsing.java
+++ LocalHost/src/localhost/HostingWithParsing.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package localhost;
 
-/**
- *
- * @author DELL
- */
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -16,8 +8,6 @@ import java.net.Socket;
 public class HostingWithParsing {
 
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-        
         ServerSocket ss = null;
         int port = 3078;
         System.err.println("Server is running on port: " + port);
@@ -32,42 +22,39 @@ public class HostingWithParsing {
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             if (ss != null && !ss.isClosed()) {
                 ss.close();
             }
         }
     }
-    
 }
 
 class ClientHandler implements Runnable {
     private BufferedReader br;
-    private InputStreamReader isr;
     private OutputStream os;
     private Socket soc;
-    
+
     public ClientHandler(Socket soc) {
         this.soc = soc;
         try {
-            isr = new InputStreamReader(soc.getInputStream());
-            br = new BufferedReader(isr);
-        } catch (Exception e) {
+            br = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
+
     @Override
     public void run() {
-        
         try {
-             String requestLine = br.readLine();
-            System.out.println("Request Line: " + requestLine);
-            
+//            String requestLine = br.readLine();
+//            System.out.println("Request Line: " + requestLine);
+
             String str;
             String host = null;
             String requestedFile = "index.html";
-            while ((str = br.readLine()) != null) {
+
+            while ((str = br.readLine()) != null ) {
                 System.out.println(str);
                 if (str.startsWith("Host:")) {
                     host = str.split(" ")[1];
@@ -81,41 +68,71 @@ class ClientHandler implements Runnable {
                         }
                     }
                 }
-
-            }
-            try (FileReader reader = new FileReader("server.txt")) {
-                os = soc.getOutputStream();
-                os.write("HTTP/1.1 200 OK\r\n".getBytes());
-                os.write("Content-Type: text/html; charset=UTF-8\r\n".getBytes());
-                os.write("\r\n".getBytes());
-                
-                int character;
-                while ((character = reader.read()) != -1) {
-                    os.write(character);
+                 if (str.isEmpty()) {
+                    break;
                 }
-                os.write("\r\n".getBytes());
-                os.flush();
-            } catch (Exception e) {
-               os = soc.getOutputStream();
-                os.write("HTTP/1.1 404 Not Found\r\n".getBytes());
-                os.write("Content-Type: text/html; charset=UTF-8\r\n".getBytes());
-                os.write("\r\n".getBytes());
-                os.write("<html><body><h1>404 Not Found</h1></body></html>".getBytes());
-                os.flush();
             }
-        } catch (Exception e) {
+
+            if (host != null) {
+                String folder = "";
+                if (host.startsWith("127.0.0.1")) {
+                    folder = "127.0.0.1";
+                } else if (host.startsWith("192.168.1.109")) {
+                    folder = "192.168.1.109";
+                }
+                serveFile(folder, requestedFile);
+            } else {
+                error();
+            }
+
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
                 if (br != null) br.close();
                 if (os != null) os.close();
-                soc.close();
+                if (soc != null && !soc.isClosed()) soc.close();
                 System.err.println("Client connection closed!");
-                
-            } catch (Exception e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-}
 
+    private void serveFile(String folder, String fileName) {
+        try (FileReader fileReader = new FileReader(folder + "/" + fileName)) {
+            os = soc.getOutputStream();
+
+            os.write("HTTP/1.1 200 OK\r\n".getBytes());
+            os.write("Content-Type: text/html; charset=UTF-8\r\n".getBytes());
+            os.write("\r\n".getBytes());
+
+            int character;
+            while ((character = fileReader.read()) != -1) {
+                os.write(character);
+            }
+            os.write("\r\n".getBytes());
+            os.flush();
+        } catch (FileNotFoundException e) {
+            
+            error();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void error() {
+        try {
+            os = soc.getOutputStream();
+
+            String response = "HTTP/1.1 404 Not Found\r\n" +
+                    "Content-Type: text/html; charset=UTF-8\r\n" +
+                    "\r\n" +
+                    "<html><body><h1>404 Not Found</h1></body></html>";
+            os.write(response.getBytes());
+            os.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
